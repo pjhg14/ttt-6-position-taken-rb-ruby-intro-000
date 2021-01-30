e9371002164ed4d0d1ad822f13cb03010fbdb195
Paul H Graham Jr <pjhg14@gmail.com>
2021-01-30 13:55:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != (" " || "" || nil)
-    true
-  else
+  if board[index] == " " || "" || nil
     false
+  else
+    true
   end
 end
